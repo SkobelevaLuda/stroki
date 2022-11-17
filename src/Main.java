5bfbbb88bbe4170e210a045f512ff589b7b219b1
Людок <skobeleva.ludmila@yandex.ru>
2022-11-17 23:46:00 +0300

    задание выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+        // задание 1 и 2.
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println(" ФИО сотрудника —  "+fullName);
+        fullName=fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета —  " +fullName);
 
-        System.out.println("Hello world!");
+        // задание 3
+        String fullName1 = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника — " +fullName1.replace("ё","е"));
+
+
     }
 }
